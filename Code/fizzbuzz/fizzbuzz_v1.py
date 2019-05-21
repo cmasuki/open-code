a0dbe1a7880c47345e51b7e022eb657b383b3ac7
Caroline Stewart <38465091+carolinestewart@users.noreply.github.com>
2019-05-21 16:36:13 +0100

    Add files via upload

---
+++ Code/fizzbuzz/fizzbuzz_v1.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 21 16:06:35 2019
+
+This program has been written to run the fizzbuzz game.
+It is designed for learning Python and for experimenting with Github.
+CMAS-open-code project
+
+@author: StewartC
+"""
+
+for a in range (1, 100):
+    if ((a%3 > 0) and (a%5 > 0)):    
+        print a
+
+    if ((a%3 == 0) and (a%5 > 0)):
+        print "Fizz"
+
+    if ((a%3 > 0) and (a%5 == 0)):
+        print "Buzz"
+    
+    if ((a%3 == 0) and (a%5 == 0)):
+        print "Fizz Buzz"    
+        
+
